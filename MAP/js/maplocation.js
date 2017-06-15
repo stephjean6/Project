40a47b2cdcf354ec36c3e02c72b72948b237c88b
stephjean6 <stephjean6@yahoo.com>
2017-06-14 21:32:50 -0700

    Add files via upload

--- MAP/js/maplocation.js
+++ MAP/js/maplocation.js
@@ -4,18 +4,23 @@ $('[name=GigHarbor]').on('click', setLocation);
 function setLocation() {
   var value =$(this).val();
   switch (value) {
-  case 'Java':
+  
+    case 'Narrows':
       $('#myMap').attr('src','http://staticmap.openstreetmap.de/staticmap.php?size=1000x800&center=47.2688,-122.5508&zoom=15&markers=47.2688,-122.5508,ol-marker-blue&maptype=mapnik');
     break;
-  case 'Jax':
+  
+    case 'Jax':
     $('#myMap').attr('src','http://staticmap.openstreetmap.de/staticmap.php?size=1000x800&center=47.32961,-122.58061&zoom=18&markers=47.32961,-122.58061,ol-marker-blue&maptype=mapnik');
     break;
-  case 'Tides':
-    $('#myMap').attr('src','http://staticmap.openstreetmap.de/staticmap.php?size=1000x800&center=47.32933,-122.57842&zoom=18&markers=47.32933,-122.57842,ol-marker-blue&maptype=mapnik');
-    break;
-  case 'Park':
+  
+    case 'Park':
     $('#myMap').attr('src','http://staticmap.openstreetmap.de/staticmap.php?size=1000x800&center=47.3081,-122.6867&zoom=15&markers=47.3081,-122.6867,ol-marker-blue&maptype=mapnik');
     break;
+  
+    case 'Tides':
+    $('#myMap').attr('src','http://staticmap.openstreetmap.de/staticmap.php?size=1000x800&center=47.32933,-122.57842&zoom=18&markers=47.32933,-122.57842,ol-marker-blue&maptype=mapnik');
+    break;
+    
   default:
     console.log('Sorry, Gig Harbor is closed.');
 }
